d7a28d7f36b5224a5b98486e014368ad79e62a79
Alexander Shutov <68386453+NuclearAlex@users.noreply.github.com>
2025-07-24 16:49:41 +0300

    Delete src directory

--- src/main/java/by/homiel/shutov/repository/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package by.homiel.shutov.repository;
-
-import by.homiel.shutov.entity.pub.QuestionEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepository extends JpaRepository<QuestionEntity, Long> {
-}
